33091750edc0177f374deb321d3dd8b3b475d78c
gracentruth <63465350+gracentruth@users.noreply.github.com>
2021-12-07 00:05:12 +0900

    Future async await

--- test/Future.dart
+++ test/Future.dart
@@ -4,10 +4,10 @@ void main() {
   showData();
 }
 
-void showData() {
+void showData() async{
   startTask();
-  accessData();
-  fetchData();
+  String account=await accessData();
+  fetchData(account);
 }
 
 void startTask() {
@@ -15,14 +15,25 @@ void startTask() {
   print(info1);
 }
 
-void accessData() {
+Future<String> accessData() async{//return할 account가 String이므
+  String account=' ';
   Duration time = Duration(seconds: 3);
-  sleep(time);
-  String info2 = '데이터접속중';
-  print(info2);
+
+  if(time.inSeconds>2){
+   // sleep(time);
+    await Future.delayed(time,(){ //await : 이부분 실행전까지 기다려라 !
+      account = '8500';
+      print(account);
+    }); //accessData()실행 전, fetchData()가 실행된다. => 즉 다음 코드가 먼저 실행됨
+
+  }else {
+    String info2 = '데이터를 가져왔습니다.';
+    print(info2);
+  }
+  return account;
 }
 
-void fetchData() {
-  String info3 = '잔액은 8500원입니';
+void fetchData(String account) {
+  String info3 = '잔액은 $account 원입니다';
   print(info3);
 }
